6d8ed52b688223bb36bcc40158fbacfabdc0381c
danielpalmeirap <danielpalmeirap@gmail.com>
2020-03-24 21:47:11 -0300

    Back-end da aplicação

---
+++ backend/src/database/migrations/20200324160510_create_ongs.js
@@ -0,0 +1,16 @@
+/* CRIA A TABELA */
+exports.up = function(knex) {
+    return knex.schema.createTable('ongs', function (table) {
+        table.string('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
+    });
+};
+
+/*DESFAZ/DELETA A TABELA SE OUVER PROBLEMA*/ 
+exports.down = function(knex) {
+    return knex.schema.dropTable('ongs');
+};
